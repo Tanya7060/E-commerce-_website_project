84ddb7401624568636f382ac18eb922b65877156
Tanya7060 <139093311+Tanya7060@users.noreply.github.com>
2024-10-25 19:40:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-// Script for navigation bar
+<script>
+  var MainImg = document.getElementById("MainImg");
+  var smallimg = documet.getElementByClassName("small-img");
+
+  smalling[0].onclick = function(){
+    MainImg.src = smalling[0].src; 
+  }
+  smalling[1].onclick = function(){
+    MainImg.src = smalling[1].src; 
+  }
+ smalling[2].onclick = function(){
+    MainImg.src = smalling[2].src; 
+  }
+ smalling[3].onclick = function(){
+    MainImg.src = smalling[3].src; 
+  }
